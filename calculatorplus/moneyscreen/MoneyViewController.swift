88287af61a5ab23b940e49a258083e2ceecbe1a5
NguyenHuuThao251 <nguyenhuuthao2001@gmail.com>
2024-03-15 15:58:09 +0700

    update layout money screen

--- calculatorplus/moneyscreen/MoneyViewController.swift
+++ calculatorplus/moneyscreen/MoneyViewController.swift
@@ -8,22 +8,63 @@
 import UIKit
 
 class MoneyViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //fetchSomeData(from: "USD", to: "VND")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonBack(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
-    */
-
+    
+    func fetchSomeData(from: String, to: String) {
+        // Tạo URL cho API endpoint bạn muốn gọi
+        let baseURL = "https://currency-exchange.p.rapidapi.com/exchange"
+        let urlString = "\(baseURL)?from=\(from)&to=\(to)&Q=1.0"
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        print(urlString)
+        // Tạo URLRequest
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // Thêm header vào yêu cầu
+        request.setValue("eeea5cd2d2msh21a7837efffc7a8p1a5317jsnb54b689dc1a3",
+                         forHTTPHeaderField: "X-RapidAPI-Key")
+        request.setValue("currency-exchange.p.rapidapi.com",
+                         forHTTPHeaderField: "X-RapidAPI-Host")
+        
+        // Tạo URLSession
+        let session = URLSession.shared
+        
+        // Tạo yêu cầu GET
+        let task = session.dataTask(with: request) { data, response, error in
+            // Kiểm tra lỗi
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            
+            guard let responseData = data else {
+                print("No data received")
+                return
+            }
+            
+            // Xử lý dữ liệu trả về, ví dụ: chuyển đổi dữ liệu thành String
+            if let responseBody = String(data: responseData, encoding: .utf8) {
+                print("Response body: \(responseBody)")
+            } else {
+                print("Unable to convert data to String")
+            }
+        }
+        
+        // Bắt đầu task
+        task.resume()
+    }
+    
 }
